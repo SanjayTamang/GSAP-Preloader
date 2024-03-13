eea3fefbcdae23ad4a16a61351a3758325d27596
Sanjay Tamang <sanjaytamang@Sanjays-MacBook-Pro-2.local>
2024-03-13 20:40:57 +0545

    update

---
+++ index.js
@@ -0,0 +1,49 @@
+const startLoader = () => {
+  const counter = document.querySelector(".counter");
+  let currentCounterValue = 0;
+
+  const updateCounter = () => {
+    if (currentCounterValue === 100) return;
+    currentCounterValue += Math.floor(Math.random() * 10) + 1;
+
+    if (currentCounterValue > 100) currentCounterValue = 100;
+
+    counter.textContent = `${currentCounterValue}%`; // Display as percentage
+
+    const delay = Math.floor(Math.random() * 200) + 50;
+    setTimeout(updateCounter, delay);
+  };
+
+  updateCounter();
+};
+
+startLoader();
+
+gsap.to(".counter", 0.25, {
+  delay: 3.5,
+  opacity: 0,
+});
+
+gsap.to(".bar", 1.5, {
+  delay: 3.5,
+  height: 0,
+  stagger: {
+    amount: 0.5,
+  },
+  ease: "power4.inOut",
+});
+
+gsap.from(".h1", 1.5, {
+  delay: 4,
+  y: 700,
+  stagger: {
+    amount: 0.5,
+  },
+  ease: "power4.inOut",
+});
+
+gsap.from(".hero", 2, {
+  delay: 4.5,
+  y: 400,
+  ease: "power4.inOut",
+});
